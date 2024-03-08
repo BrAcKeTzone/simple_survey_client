12afbe8606429f340681faee95b5c06ddd62cf0b
JhulPona <jpskiemaniwang@gmail.com>
2024-03-09 02:31:56 +0800

    first commit

---
+++ src/questionsData.jsx
@@ -0,0 +1,71 @@
+export const questionsData = [
+  {
+    id: 1,
+    question_text: "Question 1: What is your favorite color?",
+    choice_selections: ["Red", "Blue", "Green", "Yellow"],
+  },
+  {
+    id: 2,
+    question_text: "Question 2: How often do you exercise?",
+    choice_selections: ["Daily", "Weekly", "Monthly", "Rarely", "Never"],
+  },
+  {
+    id: 3,
+    question_text: "Question 3: What is your preferred mode of transportation?",
+    choice_selections: ["Car", "Bike", "Public Transit", "Walking"],
+  },
+  {
+    id: 4,
+    question_text:
+      "Question 4: Which social media platform do you use the most?",
+    choice_selections: [
+      "Facebook",
+      "Instagram",
+      "Twitter",
+      "LinkedIn",
+      "Other",
+    ],
+  },
+  {
+    id: 5,
+    question_text: "Question 5: What is your favorite food?",
+    choice_selections: ["Pizza", "Burger", "Sushi", "Pasta", "Salad"],
+  },
+  {
+    id: 6,
+    question_text: "Question 6: What is your favorite movie genre?",
+    choice_selections: ["Action", "Comedy", "Drama", "Horror", "Sci-Fi"],
+  },
+  {
+    id: 7,
+    question_text: "Question 7: How do you prefer to spend your weekends?",
+    choice_selections: [
+      "Outdoor activities",
+      "Watching movies",
+      "Reading",
+      "Socializing",
+      "Traveling",
+    ],
+  },
+  {
+    id: 8,
+    question_text: "Question 8: What type of music do you enjoy listening to?",
+    choice_selections: ["Pop", "Rock", "Hip-hop", "Classical", "Electronic"],
+  },
+  {
+    id: 9,
+    question_text: "Question 9: How do you prefer to learn new things?",
+    choice_selections: [
+      "Reading books",
+      "Online courses",
+      "Hands-on experience",
+      "Watching tutorials",
+      "Attending workshops",
+    ],
+  },
+  {
+    id: 10,
+    question_text: "Question 10: What is your favorite season?",
+    choice_selections: ["Spring", "Summer", "Autumn", "Winter"],
+  },
+];
---
+++ src/surveyAbout.jsx
@@ -0,0 +1,5 @@
+export const surveyAbout = {
+  surveyTitle: "Title of the Survey",
+  surveyInstruction:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dapibus lectus vitae risus sodales facilisis. Sed venenatis condimentum tempus. Integer molestie nibh eu lobortis hendrerit. Donec aliquam nisi eleifend, ultrices orci eu, laoreet nunc. Sed ac lectus justo.",
+};
