25cc325cae22a7c9d499050ea9b96bca8135101d
John Paul A. Maniwang <74085089+BrAcKeTzone@users.noreply.github.com>
2024-04-09 19:03:49 +0800

    Added terms and conditions template

--- src/components/surveyAbout.jsx
+++ src/components/surveyAbout.jsx
@@ -2,4 +2,6 @@ export const surveyAbout = {
   surveyTitle: "Dear WMSU students,",
   surveyInstruction:
     "We're conducting a survey to explore your preferred leisure activities beyond academics. Your participation will help us gain insights into how students at WMSU spend their free time. Please take a moment to share your interests and preferences by completing the following questionnaire. Your responses are valuable and will remain confidential. Thank you for your contribution.",
+  terms: "Terms here",
+  conditions: "Conditions Here",
 };
